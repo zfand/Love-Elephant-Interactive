2722749f20971ec4801144a6cbc79f5c70355c03
ChrisGermano <germano.c@husky.neu.edu>
2014-02-12 23:51:41 -0500

    Starting room demo
    Scene manager not yet implemented, placeholder art used

---
+++ Assets/Scripts/PlayerKeys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKeys : MonoBehaviour {
+
+	public bool white;
+	public bool green;
+	public bool yellow;
+	public bool pink;
+	public bool red;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RoomOnePickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomOnePickup : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider hit) {
+		if (hit.gameObject.tag == "RoomKey") {
+			Destroy(hit.gameObject);
+			player.GetComponent<PlayerKeys>().white = true;
+		} else if (hit.gameObject.tag == "RoomExitRight" && player.GetComponent<PlayerKeys>().white) {
+			Debug.Log ("Exit Room");
+		}
+	}
+}
