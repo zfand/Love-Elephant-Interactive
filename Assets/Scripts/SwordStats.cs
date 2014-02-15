c9c94e330f8d2029099e8e3645e870ad2b0ef3fc
duncan-macleod <troxnor@gmail.com>
2014-02-15 17:19:49 -0500

    Added swordstats.cs to sword and bfsword

--- Assets/Scripts/SwordStats.cs
+++ Assets/Scripts/SwordStats.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class SwordStats : MonoBehaviour {
 
+	public float Damage;
+	public float SwingAngle;
 	// Use this for initialization
 	void Start () {
-	
+		if(Damage == null){
+			Debug.LogError("Damage not assigned to " + this.name);
+		}
+		if(SwingAngle == null){
+			Debug.LogError("SwingAngle not assigned to " + this.name);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 }
