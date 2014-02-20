8d92016a002941bd5642fe45cc38b6c06c1c4ace
Zachary Fand <zaffand@gmail.com>
2014-02-20 11:02:36 -0500

    Cleaned up Equipment

--- Assets/Scripts/Equipment.cs
+++ Assets/Scripts/Equipment.cs
@@ -5,102 +5,87 @@ using Item;
 
 namespace LoveElephant
 {
+  /// <summary>
+  ///   Equipment.
+  ///    Items currently being used by the player.
+  /// </summary>
   public class Equipment : MonoBehaviour
   {
 
+    /// <summary>
+    /// Reference to the HookShot GameObject on the Player
+    /// </summary>
     public GameObject hookShot;
+    /// <summary>
+    /// Reference to the Weapon GameObject on the Player
+    /// </summary>
     public GameObject weapon;
+    /// <summary>
+    /// Reference to the Boot GameObject on the Player
+    /// </summary>
     public GameObject boot;
-
+    /// <summary>
+    /// Reference to the Player's Inventory
+    /// </summary>
     private Inventory box;
-    private MovementStats bootStats;
+    /// <summary>
+    /// Reference to the PlayerController
+    /// </summary>
     private PlayerController playerCrtl;
 
-    private void Awake() {
+    private void Awake()
+    {
       hookShot = null;
       weapon = null;
       boot = null;
 
-      playerCrtl = this.GetComponent<PlayerController>();
+      playerCrtl = this.GetComponent<PlayerController> ();
+      if (playerCrtl == null) {
+        Debug.LogError ("Could not find PlayerController on the Player!");
+      }
+      box = this.GetComponent<PlayerController> ();
+      if (box == null) {
+        Debug.LogError ("Could not find Inventory on the Player!");
+      }
 
-      this.transform.Cast<Transform>().ToList().ForEach(child => Equip(child.gameObject));
+      //Try to equip all items on the player
+      this.transform.Cast<Transform> ().ToList ().ForEach (child => Equip (child.gameObject));
     }
 
-    public void Equip(GameObject item) {
+    /// <summary>
+    /// Equips Item onto the Player, Unequiping existing Items
+    /// </summary>
+    public void Equip(GameObject item)
+    {
       if (item.tag == "HookShot") {
         if (hookShot != null) {
-          Unequip(hookShot);
+          Unequip (hookShot);
         }
         hookShot = item;
         hookShot.transform.parent = this.transform;
-      }
-      else if (item.tag == "Weapon") {
+      } else if (item.tag == "Weapon") {
         if (weapon != null) {
-          Unequip(weapon);
+          Unequip (weapon);
         }
         weapon = item;
         weapon.transform.parent = this.transform;
-      }
-      else if (item.tag == "Boot") {
+      } else if (item.tag == "Boot") {
         if (boot != null) {
-          Unequip(boot);
+          Unequip (boot);
         }
         boot = item;
         boot.transform.parent = this.transform;
-        playerCrtl.movementStats = boot.GetComponent<Boot>().stats;
+        playerCrtl.movementStats = boot.GetComponent<Boot> ().stats;
       }
     }
 
-    private void Unequip(GameObject item) {
+    /// <summary>
+    /// Unequips the given item then adds it to the inventory
+    /// </summary>
+    private void Unequip(GameObject item)
+    {
       item.transform.parent = null;
-      //For now
-      Destroy(item);
+      box.AddItem (item);
     }
-
-    /*
-    public void EquipWeapon(string weapon)
-    {
-      Transform weapon_parent = this.transform.FindChild ("Weapon");
-      Transform oldsword = weapon_parent.GetChild (0);
-      GameObject newsword = (GameObject)Instantiate (Resources.Load (weapon), oldsword.position, oldsword.rotation);
-      Vector3 oldpos = oldsword.transform.position;
-      float diff = Mathf.Abs (oldsword.renderer.bounds.size.x - newsword.renderer.bounds.size.x);
-      DestroyObject (oldsword.gameObject);
-      newsword.transform.position = oldpos; 
-      //newsword.transform.Translate(-diff/2, diff/2, 0);
-      newsword.transform.parent = weapon_parent;
-      
-      Quaternion old_rotation = weapon_parent.rotation;
-      weapon_parent.rotation = Quaternion.identity;
-      newsword.transform.position = new Vector3 (newsword.transform.position.x - diff,
-                                                 newsword.transform.position.y, 
-                                                 newsword.transform.position.z);
-      weapon_parent.rotation = old_rotation;
-      //Debug.Break();
-      
-      
-    }
-    
-    public void EquipBoots(string boot)
-    {
-      Transform boot_parent = this.transform.FindChild ("Boots");
-      if (boot_parent.childCount > 0) {
-        Transform oldboots = boot_parent.GetChild (0);
-        
-        if (oldboots != null) {
-          DestroyObject (oldboots.gameObject);
-        }
-      }
-      GameObject newBoots = (GameObject)Instantiate (Resources.Load ("Items/"+boot), boot_parent.position, boot_parent.rotation);
-      
-      newBoots.transform.parent = boot_parent;
-      
-      BootStats stats = newBoots.GetComponent<BootStats> ();
-      maxSpeed = originalspeed;
-      maxSpeed += stats.SpeedMod;
-      jumpForce = originalJumpForce;
-      jumpForce *= stats.JumpMod;
-    }
-    **/
   }
 }
