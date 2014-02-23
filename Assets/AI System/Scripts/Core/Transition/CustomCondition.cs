e25d2b73da53e57e517aee33a56e0b4a4e72e69b
Zachary Fand <zaffand@gmail.com>
2014-02-23 14:07:23 -0500

    removed AI manager

--- Assets/AI System/Scripts/Core/Transition/CustomCondition.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[System.Serializable]
-public class CustomCondition : ScriptableObject {
-	public virtual bool Validate(AIRuntimeController controller){
-		return false;
-	}
-}
