5a3b4abc0cf62d29cc075472e21a586ecbd3e501
ChrisGermano <germano.c@husky.neu.edu>
2014-03-03 12:59:09 -0500

    Crate thing, mesh exploder

---
+++ Assets/Scripts/Item/Crate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider hit) {
+		if (hit.gameObject.tag == "Weapon") {
+			((MeshExploder) this.GetComponent("MeshExploder")).Explode(); 
+			this.gameObject.SetActive(false);
+		}
+	}
+}
