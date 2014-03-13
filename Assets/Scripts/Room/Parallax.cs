b07a37a13b2ee5f1401839b8b58d00e1759a26cc
Chris Germano <germano.c@husky.neu.edu>
2014-03-13 11:30:17 -0400

    Updated parallax and backgrounds

---
+++ Assets/Scripts/Room/Parallax.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+  
+  private GameObject firstBackground;
+  private GameObject secondBackground;
+  private GameObject thirdBackground;
+  private Material firstMaterial;
+  private Material secondMaterial;
+  private Material thirdMaterial;
+  private Shader backShader;
+	
+  public bool flipFirst;
+  public bool flipSecond;
+  public bool flipThird;
+
+  public Texture firstTexture;
+  public Texture secondTexture;
+  public Texture thirdTexture;
+  public string shaderType;
+  public float width;
+  public float height;
+  public GameObject cam;
+
+  // Use this for initialization
+  void Start()
+  {
+    //Get player's Z to dynamically place backgrounds
+    float startDepth = GameObject.FindGameObjectWithTag ("Player").transform.position.z;
+  
+    //Create a shader based on the given type
+    backShader = new Shader ();
+    backShader = Shader.Find (shaderType);
+    
+    //Create background materials
+    firstMaterial = new Material (backShader);
+    secondMaterial = new Material (backShader);
+    thirdMaterial = new Material (backShader);
+    
+    //Apply background textures
+    firstMaterial.mainTexture = firstTexture;
+    secondMaterial.mainTexture = secondTexture;
+    thirdMaterial.mainTexture = thirdTexture;
+
+    int intHeight = (int)Mathf.Ceil (height);
+    int intWidth = (int)Mathf.Ceil (width) + 5;
+
+    //Instantiate background 1
+    firstBackground = GameObject.CreatePrimitive (PrimitiveType.Cube);
+    firstBackground.renderer.material = firstMaterial;
+    firstBackground.transform.position = new Vector3 (0, 0, startDepth - 5);
+	if (!flipFirst) {
+	  firstBackground.transform.localScale = new Vector3 (intWidth, intHeight, 1);
+	} else {
+	  firstBackground.transform.localScale = new Vector3 (-intWidth, intHeight, 1);
+	}
+    
+    //Instantiate background 2
+    secondBackground = GameObject.CreatePrimitive (PrimitiveType.Cube);
+    secondBackground.renderer.material = secondMaterial;
+    secondBackground.transform.position = new Vector3 (0, 0, startDepth + 5);
+	if (!flipSecond) {
+    	secondBackground.transform.localScale = new Vector3 (intWidth, intHeight, 1);
+	} else {
+		secondBackground.transform.localScale = new Vector3 (-intWidth, intHeight, 1);
+	}
+
+    //Instantiate background 3
+    thirdBackground = GameObject.CreatePrimitive (PrimitiveType.Cube);
+    thirdBackground.renderer.material = thirdMaterial;
+    thirdBackground.transform.position = new Vector3 (0, 0, startDepth + 10);
+	if (!flipThird) {
+      thirdBackground.transform.localScale = new Vector3 (intWidth, intHeight, 1);
+	} else {
+	  thirdBackground.transform.localScale = new Vector3 (-intWidth, intHeight, 1);
+	}
+    
+    //Remove background colliders
+    firstBackground.collider.isTrigger = true;
+    secondBackground.collider.isTrigger = true;
+    thirdBackground.collider.isTrigger = true;
+    
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+    if (enabled) {
+      float camX = cam.transform.position.x;
+    
+      firstBackground.transform.position = new Vector3 (0, 0, firstBackground.transform.position.z);
+      secondBackground.transform.position = new Vector3 (0, 0, secondBackground.transform.position.z);
+      thirdBackground.transform.position = new Vector3 (0, 0, thirdBackground.transform.position.z);
+    
+      Vector3 newFirstPos = new Vector3 (firstBackground.transform.position.x - camX / 4,
+                                  firstBackground.transform.position.y,
+                                        firstBackground.transform.position.z);
+    
+      Vector3 newSecondPos = new Vector3 (secondBackground.transform.position.x - camX / 6,
+                                   secondBackground.transform.position.y,
+                                         secondBackground.transform.position.z);
+    
+      Vector3 newThirdPos = new Vector3 (thirdBackground.transform.position.x - camX / 8,
+                                  thirdBackground.transform.position.y,
+                                        thirdBackground.transform.position.z);
+    
+      firstBackground.transform.position = newFirstPos;
+      secondBackground.transform.position = newSecondPos;
+      thirdBackground.transform.position = newThirdPos;
+    }
+  }
+}
