31f87f8315159e29fe77361b3123b57d2ef3a166
duncan-macleod <troxnor@gmail.com>
2014-03-18 20:55:18 -0400

    Added GnG animations and texture

--- Assets/Scripts/GreedRoom/GreedGluttonyAI.cs
+++ Assets/Scripts/GreedRoom/GreedGluttonyAI.cs
@@ -3,23 +3,16 @@ using System.Collections;
 
 public class GreedGluttonyAI : MonoBehaviour {
 
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
+		anim.SetTrigger("Walk");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.transform.position = new Vector3(this.transform.position.x + 0.1f, this.transform.position.y, this.transform.position.z);
 	}
 
-
-	void OnCollisionEnter(Collision c){
-		Debug.Log ("Testing");
-		Debug.Log(c.collider.name);
-	}
-	void OnTriggerEnter(Collider c){
-		Debug.Log ("Testing");
-		Debug.Log(c.collider.name);
-	}
 }
