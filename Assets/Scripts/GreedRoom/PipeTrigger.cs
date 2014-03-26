01d939d0c04f6d25d135935db14aeb84d1488bae
ChrisGermano <germano.c@husky.neu.edu>
2014-03-26 17:07:46 -0400

    Large Green Wing updates

---
+++ Assets/Scripts/GreedRoom/PipeTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using LoveElephant;
+
+namespace Boss
+{
+	public class PipeTrigger : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnTriggerEnter(Collider hit) {
+			if (hit.gameObject.tag == "PipeTrigger") {
+				foreach (GameObject p in GameObject.FindGameObjectsWithTag("Pipe")) {
+					p.GetComponent<Pipe>().Open ();
+				}
+			}
+		}
+	}
+}
