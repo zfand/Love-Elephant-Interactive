b75cc40bee7046d9ee5d6652f80637e227634e2e
duncan-macleod <troxnor@gmail.com>
2014-03-27 18:44:43 -0400

    Added vomit loop

---
+++ Assets/StrongStomp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using LoveElephant;
+using System.Collections;
+
+public class StrongStomp : MonoBehaviour {
+
+	public float Damage = 20f;
+	public float ExplosiveForce = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if(coll.gameObject.CompareTag("Player")){
+			coll.GetComponent<PlayerStats>().TakeDamage(Damage);
+			coll.rigidbody.AddExplosionForce(ExplosiveForce, this.transform.position, transform.GetComponent<SphereCollider>().radius);
+		}
+	}
+}
