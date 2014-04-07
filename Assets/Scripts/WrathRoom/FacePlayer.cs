c72de146293598691b1152d72415e15aac944a44
ChrisGermano <germano.c@husky.neu.edu>
2014-04-07 17:33:03 -0400

    Wrath prototype (do not touch!)

---
+++ Assets/Scripts/WrathRoom/FacePlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacePlayer : MonoBehaviour {
+
+	GameObject player;
+	public float speed = 0.5f;
+	private Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		targetRotation = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		targetRotation = Quaternion.LookRotation (player.transform.position - this.transform.position);
+		
+		float str = Mathf.Min (speed * Time.deltaTime, 1);
+		
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, str);
+		
+	}
+}
---
+++ Assets/Scripts/WrathRoom/WrathAttack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WrathAttack : MonoBehaviour {
+
+	bool shot = false;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Animation>().PlayQueued("Wrath_Shoot", QueueMode.PlayNow);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
