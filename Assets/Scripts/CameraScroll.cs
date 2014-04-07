be1b5d439226f18b047858d576fd0fba04498a64
Zachary Fand <zaffand@gmail.com>
2014-04-07 12:15:46 -0400

    updated horizontal scrolling, now it slowly moves up and down

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -6,6 +6,7 @@ public class CameraScroll : MonoBehaviour
 
   public GameObject cam;
   public float horizontalLimit;
+  public float hMoveTime = 0.1f;
   private GameObject player;
   private float playerStartY;
 
@@ -23,32 +24,58 @@ public class CameraScroll : MonoBehaviour
 
     Ray leftRay = cam.camera.ScreenPointToRay (new Vector3 (0f, 0f));
     Ray rightRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth, 0f));
-    Ray upRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, cam.camera.pixelHeight));
-    Ray downRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, 0f));
 
     float xdir = cam.transform.position.x - player.transform.position.x;
     float ydir = cam.transform.position.y - player.transform.position.y;
 
-    Debug.Log(xdir);
-
+    //move vertically
     if (xdir > 0 && Physics.Raycast (leftRay)) {
-      Debug.Log("L");
       pos.x = player.transform.position.x;
     } 
 
     if (xdir < 0 && Physics.Raycast (rightRay)) {
-      Debug.Log("R");
       pos.x = player.transform.position.x;
     }
 
-    /*
-    if (horizontalLimit > 0 && Mathf.Abs(playerStartY - player.transform.position.y) > horizontalLimit) {
-      Debug.Log("U");
-      pos.y = player.transform.position.y;
-      playerStartY = pos.y;
-    }
-    */
-
     cam.transform.position = pos;
+
+    //move horizonally
+    if (horizontalLimit > 0) {
+      if(playerStartY + horizontalLimit < player.transform.position.y) {
+        pos.y += horizontalLimit;
+        playerStartY = player.transform.position.y;
+        StopCoroutine("HMoveCam");
+        StartCoroutine("HMoveCam", pos);
+      }
+      if(playerStartY - horizontalLimit > player.transform.position.y) {
+        pos.y -= horizontalLimit;
+        playerStartY = player.transform.position.y;
+        StopCoroutine("HMoveCam");
+        StartCoroutine("HMoveCam", pos);
+      }
+    }
+
+  }
+
+  private IEnumerator HMoveCam(Vector3 pos) {
+    float deltaTime = 0;
+    float startY = cam.transform.position.y;
+    Ray ray;
+    if (cam.transform.position.y - pos.y <= 0) {
+      ray = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, cam.camera.pixelHeight));
+    } else {
+      ray = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, 0f));
+    }
+
+
+    while (deltaTime <= hMoveTime) {
+      if (Physics.Raycast (ray)) {
+      Vector3 newPos = cam.transform.position;
+        newPos.y = Mathf.Lerp(startY, pos.y, deltaTime / hMoveTime);
+        cam.transform.position = newPos;
+        deltaTime += Time.deltaTime;
+      }
+      yield return 0;
+    }
   }
 }
