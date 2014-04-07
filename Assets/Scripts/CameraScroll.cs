f7bbc56d2c07f49139f42b7952deaa42a16d8830
Zachary Fand <zaffand@gmail.com>
2014-04-07 10:55:38 -0400

    changed how scrolling works, still buggy -starting cam pos is borked -changing the y position is borked

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -1,51 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraScroll : MonoBehaviour {
+public class CameraScroll : MonoBehaviour
+{
 
-	public GameObject cam;
-	public float widthBarrier;
-	public bool TheHub;
-	public bool tallRoom;
+  public GameObject cam;
+  public float horizontalLimit;
+  private GameObject player;
+  private float playerStartY;
 
-	// Use this for initialization
-	void Start () {
-		cam.transform.position = new Vector3(widthBarrier, cam.transform.position.y, cam.transform.position.z);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  // Use this for initialization
+  void Start()
+  {
+    player = GameObject.FindGameObjectWithTag ("Player");
+    playerStartY = player.transform.position.y;
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+    Vector3 pos = cam.transform.position;
 
-		float playerX = GameObject.FindGameObjectWithTag ("Player").transform.position.x;
-		float playerY = GameObject.FindGameObjectWithTag ("Player").transform.position.y;
+    Ray leftRay = cam.camera.ScreenPointToRay (new Vector3 (0f, 0f));
+    Ray rightRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth, 0f));
+    Ray upRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, cam.camera.pixelHeight));
+    Ray downRay = cam.camera.ScreenPointToRay (new Vector3 (cam.camera.pixelWidth/2, 0f));
 
-		float newX = playerX;
-		float newY = cam.transform.position.y;
+    float xdir = cam.transform.position.x - player.transform.position.x;
+    float ydir = cam.transform.position.y - player.transform.position.y;
 
-		if (tallRoom) {
-			newY = playerY + 6.5f;
-		}
+    Debug.Log(xdir);
 
-		if (newX < -widthBarrier) {
-			newX = -widthBarrier;
-		} else if (newX > widthBarrier) {
-			newX = widthBarrier;
-		}
-		
-		if (newY > widthBarrier + 6.5f) {
-			newY = widthBarrier + 6.5f;
-		}
+    if (xdir > 0 && Physics.Raycast (leftRay)) {
+      Debug.Log("L");
+      pos.x = player.transform.position.x;
+    } 
 
-		if (TheHub && newY > 6.5f) {
-			newY = 6.5f;
-		}
+    if (xdir < 0 && Physics.Raycast (rightRay)) {
+      Debug.Log("R");
+      pos.x = player.transform.position.x;
+    }
 
-		if (playerX > -widthBarrier && playerX < widthBarrier) {
-			cam.transform.position = new Vector3(playerX, newY, cam.transform.position.z);
-		} else if (playerX < -widthBarrier) {
-			cam.transform.position = new Vector3(-widthBarrier, newY, cam.transform.position.z);
-		} else if (playerX > widthBarrier) {
-			cam.transform.position = new Vector3(widthBarrier, newY, cam.transform.position.z);
-		}
-	}
+    /*
+    if (horizontalLimit > 0 && Mathf.Abs(playerStartY - player.transform.position.y) > horizontalLimit) {
+      Debug.Log("U");
+      pos.y = player.transform.position.y;
+      playerStartY = pos.y;
+    }
+    */
+
+    cam.transform.position = pos;
+  }
 }
