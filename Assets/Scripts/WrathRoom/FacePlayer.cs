ff1e8ffdc6454d24265a1bd08944e43a9b8a7fc2
Chris Germano <germano.c@husky.neu.edu>
2014-04-08 08:54:59 -0400

    Temp wrath updates

--- Assets/Scripts/WrathRoom/FacePlayer.cs
+++ Assets/Scripts/WrathRoom/FacePlayer.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FacePlayer : MonoBehaviour {
 
+	public GameObject chargeShot;
+	public GameObject beam;
 	GameObject player;
 	public float speed = 0.5f;
 	private Quaternion targetRotation;
@@ -22,8 +24,6 @@ public class FacePlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!firing) {
-			//if (!GetComponent<Animation>().isPlaying("Wrath_Idle"))
-			GetComponent<Animation>().Play ("Wrath_Idle", PlayMode.StopAll);
 			targetRotation = Quaternion.LookRotation (player.transform.position - this.transform.position);
 			float str = Mathf.Min (speed * Time.deltaTime, 1);
 			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, str);
@@ -34,9 +34,15 @@ public class FacePlayer : MonoBehaviour {
 	IEnumerator LookAndFire() {
 		while (true) {
 			firing = true;
-			yield return new WaitForSeconds(shotSpeed);
 			GetComponent<Animation>().Play ("Wrath_Shoot", PlayMode.StopAll);
+			chargeShot.renderer.enabled = true;
+			beam.particleSystem.Play ();
+			yield return new WaitForSeconds(shotSpeed/2f);
 			firing = false;
+			chargeShot.renderer.enabled = false;
+			yield return new WaitForSeconds(shotSpeed);
+			beam.particleSystem.Stop ();
+			GetComponent<Animation>().Play();
 
 		}
 	}
