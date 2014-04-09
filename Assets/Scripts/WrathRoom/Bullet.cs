e496861e1ba67ffa53de6f0d1769029928c47a36
ChrisGermano <germano.c@husky.neu.edu>
2014-04-09 08:03:08 -0400

    Removed extra players from rooms

---
+++ Assets/Scripts/WrathRoom/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector3.forward * Time.deltaTime * 5);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "floor" ||
+		    col.gameObject.tag == "wall" ||
+		    col.gameObject.tag == "ceiling")
+			Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/WrathRoom/Turret.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	GameObject player;
+
+	public int speed;
+	public int shootFreq;
+	public float projSpeed;
+	public GameObject bullet;
+
+	private Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		StartCoroutine(Shoot());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		targetRotation = Quaternion.LookRotation (player.transform.position - this.transform.position);
+		float str = Mathf.Min (speed * Time.deltaTime, 1);
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, str);
+	}
+
+	IEnumerator Shoot() {
+		yield return new WaitForSeconds(shootFreq);
+		Instantiate(bullet, this.transform.position, this.transform.rotation);
+
+	}
+
+}
