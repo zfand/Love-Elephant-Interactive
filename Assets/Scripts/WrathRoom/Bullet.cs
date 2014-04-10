2755369275d2d92d50c77cc3c7d53306f9c4340b
ChrisGermano <germano.c@husky.neu.edu>
2014-04-10 01:49:05 -0400

    Add bullet damage/knockback

--- Assets/Scripts/WrathRoom/Bullet.cs
+++ Assets/Scripts/WrathRoom/Bullet.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 namespace LoveElephant {
 	public class Bullet : MonoBehaviour {
+		
+		GameObject player;
+		public int boomPower;
 
 		// Use this for initialization
 		void Start () {
-		
+			player = GameObject.FindGameObjectWithTag("Player");
 		}
 		
 		// Update is called once per frame
@@ -16,6 +19,9 @@ namespace LoveElephant {
 
 		void OnCollisionEnter(Collision col) {
 			if (!(col.gameObject.CompareTag("Turret") || col.gameObject.CompareTag("Bullet"))) {
+				player.rigidbody.AddExplosionForce (boomPower, this.transform.position, 3f);
+				if (Vector3.Distance (this.transform.position, player.transform.position) < 2)
+					player.GetComponent<PlayerStats>().TakeDamage (5);
 				this.GetComponent<MeshExploder>().Explode();
 				Destroy (this.gameObject);
 			}
